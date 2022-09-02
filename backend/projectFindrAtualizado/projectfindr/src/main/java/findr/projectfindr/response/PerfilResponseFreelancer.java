102d6fff671193cdbddc77ba9b613023f31e01f6
breno fraga <breno.anjos@bandtec.com.br>
2022-09-02 20:57:27 -0300

    att back

---
+++ backend/projectFindrAtualizado/projectfindr/src/main/java/findr/projectfindr/response/PerfilResponseFreelancer.java
@@ -0,0 +1,18 @@
+package findr.projectfindr.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PerfilResponseFreelancer {
+    private String name;
+    private byte[] image;
+    private String city;
+    private String state;
+    private String email;
+    private String technologyUsed;
+
+}
