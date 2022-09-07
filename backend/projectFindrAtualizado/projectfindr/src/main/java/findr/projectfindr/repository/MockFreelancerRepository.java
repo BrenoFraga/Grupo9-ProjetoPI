f2f71b4ae57236cd1bdb4a497e709395c05c1190
BrenoFraga <breno.anjos@bandtec.com.br>
2022-09-07 14:31:21 -0300

    back

--- backend/projectFindrAtualizado/projectfindr/src/main/java/findr/projectfindr/repository/MockFreelancerRepository.java
+++
@@ -1,7 +0,0 @@
-package findr.projectfindr.repository;
-
-import findr.projectfindr.model.MockFreelancer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MockFreelancerRepository extends JpaRepository<MockFreelancer,Long> {
-}
